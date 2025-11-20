77b0db52cee329099dbbacc87b1f9bfb2c6a4bb4
HeyReyHR <capitainrobert1234@gmail.com>
2025-11-21 00:35:13 +0300

    fix

--- internal/transport/api/handlers/auth.go
+++ internal/transport/api/handlers/auth.go
@@ -18,17 +18,21 @@ import (
 
 type Auth struct {
 	userService service.UserService
+
 	authService service.AuthService
 
 	logger *infra.Logger
 }
 
 // NewAuth - создать новый экземпляр обработчика
+
 func NewAuth(userService *user.Service, authService *auth.Service, logger *infra.Logger, router *echo.Echo) *Auth {
 	result := &Auth{
 		userService: userService,
+
 		authService: authService,
-		logger:      logger,
+
+		logger: logger,
 	}
 
 	router.POST("/api/login", result.login)
@@ -37,19 +41,32 @@ func NewAuth(userService *user.Service, authService *auth.Service, logger *infra
 }
 
 // login godoc
+
 // @Summary      Login
+
 // @Description  Войти в аккаунт, также выполняет функцию регистрации
+
 // @Tags         auth
+
 // @Accept       json
+
 // @Produce      json
+
 // @Param        body body dto.AuthData  true  "Auth data"
+
 // @Success      200  {object}  dto.Token
+
 // @Failure      400  {object}  dto.ApiError
+
 // @Failure      401  {object}  dto.ApiError
+
 // @Failure      500  {object}  dto.ApiError
+
 // @Router       /api/login [post]
+
 func (h *Auth) login(echoCtx echo.Context) error {
 	var data dto.AuthData
+
 	if err := echoCtx.Bind(&data); err != nil {
 		return err
 	}
@@ -59,37 +76,51 @@ func (h *Auth) login(echoCtx echo.Context) error {
 	h.logger.Info("login: " + data.Email)
 
 	var err error
+
 	var userID string
 
 	user, err := h.userService.GetByEmail(ctx, data.Email)
+
 	if err != nil {
+
 		if !errors.Is(err, gorm.ErrRecordNotFound) {
+
 			h.logger.Warn(fmt.Sprintf("login error: email - %s, error - %s", data.Email, err.Error()))
 
 			return utils.Convert(err, h.logger)
+
 		}
 
 		userID, err = h.userService.Create(ctx, data.Email, data.Password)
 		if err != nil {
+
 			h.logger.Warn(fmt.Sprintf("login error: email - %s, error - %s", data.Email, err.Error()))
 
 			return utils.Convert(err, h.logger)
+
 		}
+
 	} else {
+
 		if err = h.authService.VerifyPassword(user, data.Password); err != nil {
+
 			h.logger.Warn(fmt.Sprintf("login error: email - %s, error - %s", data.Email, err.Error()))
 
 			return utils.Convert(err, h.logger)
+
 		}
 
 		userID = user.ID
+
 	}
 
 	token, err := h.authService.GenerateToken(userID)
 	if err != nil {
+
 		h.logger.Warn(fmt.Sprintf("login error: email - %s, error - %s", data.Email, err.Error()))
 
 		return utils.Convert(err, h.logger)
+
 	}
 
 	tokenData := dto.Token{
