0539552bfcb3140df925f9511385f307fe00ac46
linuxfight <kiko13024@gmail.com>
2025-11-21 18:35:40 +0300

    fix linter

--- cmd/main.go
+++ cmd/main.go
@@ -24,18 +24,18 @@ import (
 // @description "Type 'Bearer TOKEN' to correctly set the API Key"
 func main() {
 	// TODO: log db requests
-  // TODO: add tracing, logging and metrics
+	// TODO: add tracing, logging and metrics
 
 	cfg, err := infra.NewConfig()
 	if err != nil {
 		panic(err)
 	}
-  
+
 	logger, err := infra.NewLogger(cfg)
 	if err != nil {
 		panic(err)
 	}
-  
+
 	fx.New(
 		fx.Supply(logger.Zap, logger, cfg),
 
